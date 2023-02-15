942fc509da458fca841448509eab5cdd64d2a4a2
Jani Kaartinen <jkaart@gmail.com>
2023-02-15 11:56:28 +0200

    rest api mokki taululla

--- index.js
+++ index.js
@@ -7,8 +7,60 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 
+let iptable = [
+  {
+    "user" : "jorma",
+    "id" : 1,
+    "ips" : [
+      {
+        "ip" : "10.36.64.101",
+        "desc" : "server 1",
+        "id" : 1
+      },
+      {
+        "ip" : "10.36.64.102",
+        "desc" : "server 2",
+        "id" : 2
+      }
+    ],
+  },
+  {
+    "user" : "jaska",
+    "id" : 2,
+    "ips" : [
+      {
+        "ip" : "10.36.64.103",
+        "desc" : "server 1",
+        "id" : 1
+      },
+      {
+        "ip" : "10.36.64.104",
+        "desc" : "server 2",
+        "id" : 2
+      }
+    ],
+  }
+]
+
+
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/iptable/', (request, response) => {
+  response.json(iptable)
+
+})
+
+app.get('/api/iptable/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const user = iptable.find(user => user.id === id)
+  if (user) {
+    response.json(user)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = process.env.PORT || 3001
