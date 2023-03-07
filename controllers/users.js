3c5f7acb84a160f11c5c0655c5075737530e0650
Jani Kaartinen <jkaart@gmail.com>
2023-03-07 14:03:17 +0200

    Käytäjän poisto ei toimi

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
+const IPs = require('../models/ip')
 const adminAuth = require('../utils/adminAuth')
 
 usersRouter.post('/', async (request, response) => {
@@ -25,6 +26,16 @@ usersRouter.get('/',adminAuth, async (request, response) => {
   const users = await User
     .find({}).populate('ips', { ip: 1, desc: 1, expirationDate: 1, createdAt: 1 })
   response.json(users)
+})
+
+//Poistetaan käyttäjä
+usersRouter.delete('/:id',adminAuth, async (request, response) => {
+  const user = request.params.id
+  //poistetaan IP-taulusta kyseisen käyttäjän kaikki IP:t
+  
+  //poistetaan itse käyttäjä
+//  await User.deleteOne({id: user})
+  response.status(204).end()
 })
 
 module.exports = usersRouter
