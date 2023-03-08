8a2591201b9b61d2ab499bab5be142d3c0f51aee
Jani Kaartinen <jkaart@gmail.com>
2023-03-08 18:18:25 +0200

    Collectionien luonti jos oletus käyttäjää ei ole.

--- controllers/createdefaultuser.js
+++ controllers/createdefaultuser.js
@@ -3,7 +3,10 @@ const config = require('../utils/config')
 const logger = require('../utils/logger')
 const readline = require('readline')
 const mongoose = require('mongoose')
+
 const User = require('../models/user')
+const IPs = require('../models/ip')
+const Network = require('../models/network')
 
 mongoose.set('strictQuery', true)
 
@@ -16,13 +19,27 @@ const connectToDatabase = async () => {
   try {
     await mongoose.connect(config.MONGODB_URI)
     logger.info('connected to MongoDB')
-
     const numUsers = await User.countDocuments()
     if (numUsers > 0) {
       logger.error('There are already users in the database, skipping admin user creation')
       await mongoose.disconnect()
       process.exit()
     }
+    //Luodaan User malllin mukainen collection
+    User.createCollection()
+      .then(collection => {
+        logger.info('User collection is created!')
+      })
+    //Luodaan IPs malllin mukainen collection
+    IPs.createCollection()
+      .then(collection => {
+        logger.info('IPs collection is created!')
+      })
+    //Luodaan Network malllin mukainen collection
+    Network.createCollection()
+      .then(collection => {
+        logger.info('Network collection is created!')
+      })
 
     rl.question('Enter admin email: ', async (email) => {
       await rl.question('Enter admin name: ', async (name) => {
