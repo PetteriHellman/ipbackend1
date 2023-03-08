a12a53c4d82fa340884a3101984d41829c0d40d1
Jani Kaartinen <jkaart@gmail.com>
2023-03-08 11:00:09 +0200

    Kommentti lisätty

--- controllers/ips.js
+++ controllers/ips.js
@@ -109,7 +109,7 @@ ipsRouter.post('/next-ip',userAuth, async (request, response, next) => {
     })
 })
 
-
+//Vahvistetaan automaattisesti generoitu IP oikealla vanhenemisajalla
 ipsRouter.put('/next-ip/:id',userAuth, async (request, response, next) => {
   const body = request.body
   //Otetaan kirjautuneen käyttäjän tiedot talteen
