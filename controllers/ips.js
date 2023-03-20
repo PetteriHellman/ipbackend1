a0e21701a09b97e92affb707e0b0647262f5e569
joonas-ris <joonas.rissanen@outlook.com>
2023-03-20 09:55:24 +0200

    nextFreeIPBlock implementoitu

--- controllers/ips.js
+++ controllers/ips.js
@@ -62,51 +62,88 @@ const randomIP = (hostMin, hostMax, network) => {
   return ipString
 }
 
-const getNextIp = (networkId) => {
+//haetaan seuraava vapaa viereikkäin oleva IP-blokki 
+const nextFreeIPBlock = (size, network, taken) => {
+    const max = ip.toLong(network.hostMax)
+    const min = ip.toLong(network.hostMin)
+
+    if (size < 1 || size > 5) size = 1; //tarkistus, että haetaan vain 1-5 IP:tä
+
+    let start = 0;
+    let possible;
+
+    for (i = min; i < max; i++) 
+    {
+        if (!taken.includes(i)) 
+        {
+            start++;
+            if(start==1) possible = i;
+            //console.log(start);
+        } 
+        else start = 0;
+        if (start == size)
+        {
+            return Array(size).fill().map((_, index) => intToIP(possible + index))
+        }
+    }
+    return false;
+}
+
+const getNextIp = (networkId, taken, amount) => {
   //Etsitään verkkon tiedot kannasta
   return Network.findById(networkId)
     .then(network => {
       if (!network) {
         throw new Error('Network not found')
       }
+      return nextFreeIPBlock(amount, network, taken);
       //Luodaan satunnainen IP-osoite
-      const ipAddress = randomIP(network.hostMin, network.hostMax, network.hostNetwork)
+      //const ipAddress = randomIP(network.hostMin, network.hostMax, network.hostNetwork)
+      //console.log(ipAddress);
       //Etsitään kannasta että löytyykö juuri luotu IP
-      return IPs.findOne({ ip: ipAddress }).limit(1)
-        .then(existingIp => {
-          if (existingIp) {
-            //Jos kannasta löytyy jo kyseinen osoite ajetaan sama funktio uudelleen
-            return getNextIp(networkId)
-          } else {
-            //Jos kannasta ei löydy kyseistä osoistetta palautetaan se
-            return ipAddress
-          }
-        })
+    //   return IPs.findOne({ ip: ipAddress }).limit(1)
+    //     .then(existingIp => {
+    //       if (existingIp) {
+    //         //Jos kannasta löytyy jo kyseinen osoite ajetaan sama funktio uudelleen
+    //         return getNextIp(networkId)
+    //       } else {
+    //         //Jos kannasta ei löydy kyseistä osoistetta palautetaan se
+    //         return ipAddress
+    //       }
+    //     })
     })
 }
 
 //Tarjotaan uutta satunnaisesti generoitua IP-osoitetta
 ipsRouter.post('/next-ip',auth, async (request, response, next) => {
   const body = request.body
-  
+  const amount = body.amount
+  //Haetaan kaikki jo varatut IP:t
+  const taken = (await IPs.find({})).map((item) => (ip.toLong(item.ip)));
   //Otetaan kirjautuneen käyttäjän tiedot talteen
   const user = await User.findById(request.decodedToken.id)
   //Kutsutaan getNextIp funktiota
-  getNextIp(body.networkId)
+  getNextIp(body.networkId, taken, amount)
     .then(async ipAddress => {
       //const expireDate = Date.now() + body.TTL * 86400 * 1000
       //Asetetaan aika ensiksi 10 minuutiksi ja päivitetään oikea aika sitten vasta kun käyttäjä hyväksyy IP:n
       const expireDate = Date.now() + 600 * 1000
-      const ip = new IPs({
-        desc: body.desc,
-        ip: ipAddress,
-        user: user._id,
-        expirationDate: expireDate,
-      })
-      //Varataan IP-osoite käyttöön
-      const savedIP = await ip.save()
+      const ip = [];
+      ipAddress.forEach(e => { //pusketaan haetut IP:t (string) muotoiltuun objekti-taulukkoon (IPs)
+        ip.push(new IPs({
+          desc: body.desc,
+          ip: e,
+          user: user._id,
+          expirationDate: expireDate,
+        }))
+      });
+      //Varataan IP-osoitteet käyttöön
+      const savedIP = await IPs.insertMany(ip)
+      savedIP.forEach((e) => {
+        user.ips.push(e._id)
+      });
 
-      user.ips = user.ips.concat(savedIP._id)
+      //user.ips = user.ips.concat(savedIP._id)
       await user.save()
       //Palautetaan varattu IP-osoite
       response.status(201).json({ message: 'Uusi IP-osoite luotu', savedIP })
@@ -174,3 +211,12 @@ ipsRouter.put('/:id',auth, async (request, response, next) => {
 })
 
 module.exports = ipsRouter
+
+function intToIP(int) {
+    var part1 = int & 255;
+    var part2 = ((int >> 8) & 255);
+    var part3 = ((int >> 16) & 255);
+    var part4 = ((int >> 24) & 255);
+
+    return part4 + "." + part3 + "." + part2 + "." + part1;
+}
