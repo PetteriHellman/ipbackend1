c686598a167eb12b0cfa7ffa31416feb1113b1ea
Jani Kaartinen <jkaart@gmail.com>
2023-03-23 10:43:19 +0200

    swagger adds

--- utils/swagger.js
+++ utils/swagger.js
@@ -19,16 +19,73 @@ const doc = {
         scheme: 'bearer',
         bearerFormat: 'JWT',
       }
-    }
-  },
-  definitions: {
-    role: {
-      '@enum': [
-        'admin',
-        'user'
-      ]
-    }
-  },          // by default: empty object (Swagger 2.0)
+    },
+    schemas: {
+      User: {
+        name: {
+          type: 'string',
+          example: 'John Doe'
+        },
+        email: {
+          type: 'string',
+          example: 'john.doe@mail.com'
+        },
+        password: {
+          type: 'string',
+          example: '12345678'
+        },
+        group: {
+          type: 'string',
+          example: 'SomeGroup'
+        }
+      },
+      IP: {
+        ip: {
+          type: 'string',
+          example: '192.168.0.1'
+        },
+        desc: {
+          type: 'string',
+          example: 'Some description'
+        },
+        TTL: {
+          type: 'number',
+          example: '1'
+        },
+      },
+      Network: {
+        networkName: {
+          type: 'string',
+          example: 'SomeNetwork'
+        },
+        hostMin: {
+          type: 'string',
+          example: '192.168.0.100'
+        },
+        hostMax: {
+          type: 'string',
+          example: '192.168.0.200'
+        },
+        hostNetwork: {
+          type: 'string',
+          example: '24'
+        },
+        networkActive: {
+          type: 'boolean',
+          example: 'true'
+        }
+      }
+    },
+    definitions: {
+      role: {
+        '@enum': [
+          'admin',
+          'user',
+          'null'
+        ]
+      }
+    },
+  }
 }
 
 const outputFile = './docs/swagger-output.json'
