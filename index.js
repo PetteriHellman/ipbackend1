b224f4fb646546e2531af00e355a989acbaeb1e8
Petteri Hellman <petteri.hellman@gmail.com>
2023-03-24 10:43:29 +0200

    build again

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = require('./app')
-const config = require('./utils/config')
+// const config = require('./utils/config')
 const logger = require('./utils/logger')
 const PORT = process.env.PORT || 3001
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  logger.info('Server running on port ${PORT}')
 })
